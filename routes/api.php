630caac838b1a538c41b1772bd92900da2f81901
Gde <gugumgde26@gmail.com>
2025-05-20 20:43:00 +0700

    menambah routes untuk aplikasi mobile authentikasi

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Mobile\AuthController as MobileAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Mobile Authentication Routes
+Route::post('/mobile/login', [MobileAuthController::class, 'login']);
+Route::post('/mobile/logout', [MobileAuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/mobile/user', [MobileAuthController::class, 'user'])->middleware('auth:sanctum');
+
+// Web Authentication Routes
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
